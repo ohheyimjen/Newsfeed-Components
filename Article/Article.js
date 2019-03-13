d48b6990768c25b27b2de9adb799b30c040a0e2c
ohheyimjen <jendority@gmail.com>
2019-03-13 15:43:58 -0700

    Completed all but ' Using your expandButton reference, update the text on your expandButton to say expand' under Article.js

--- Article/Article.js
+++ Article/Article.js
@@ -3,20 +3,31 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
+    // const expand = Array.from(document.querySelectorAll('.expandButton'));
+
     
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+
+    this.expandButton.addEventListener('click', () => this.expandArticle());
+
+    contentHeaders[5].textContent = 'Contact';
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.expandButton.classList.toggle('article');
   }
 }
+
+// togglePanel() {
+//   this.panelBtnOpen.classList.toggle('hide-btn');
+//   this.panelBtnClose.classList.toggle('hide-btn');
+//   this.panelContent.classList.toggle('panel-content');
+// }
 
 /* START HERE: 
 
@@ -26,4 +37,48 @@ class Article {
 
 */
 
-let articles;
+let articles = document.querySelectorAll('.article');
+
+articles.forEach(function(article) {
+  return new Article(article);
+})
+
+
+
+// panels.forEach(function(panel) {
+//   return new Panel(panel);
+// })
+
+
+
+
+
+
+
+
+// //step 3
+// class Panel {
+//   constructor(panel) {
+//     this.panel = panel;
+//     this.panelButtons = this.panel.querySelector('.panel-buttons');
+//     this.panelBtnOpen = this.panel.querySelector('.panel-btn-open');
+//     this.panelBtnClose = this.panel.querySelector('.panel-btn-close');
+//     this.panelContent = this.panel.querySelector('.panel-content');
+
+    step 4 add event listener
+    this.panelButtons.addEventListener('click', this.togglePanel.bind(this));
+    this.panelButtons.addEventListener('click', () => this.togglePanel());
+//   }
+//   togglePanel() {
+//     this.panelBtnOpen.classList.toggle('hide-btn');
+//     this.panelBtnClose.classList.toggle('hide-btn');
+//     this.panelContent.classList.toggle('panel-content');
+//   }
+// }
+// //step 1 get original DOM object
+// const panels = document.querySelectorAll('.panel');
+
+// //step 2 return a newly constructed DOM element
+// panels.forEach(function(panel) {
+//   return new Panel(panel);
+// })
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,11 @@
 
 const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
+  // Toggle the "menu--open" class on your menu reference. 
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelectorAll('.menu');
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelectorAll('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
+this.menuButton.addEventListener('click', () => this.toggleMenu());
